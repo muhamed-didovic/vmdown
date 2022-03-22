ec58e53b58ce19228898938d724a4e58c9ba4777
Muhamed Didovic <muhamed.didovic@gmail.com>
2022-03-22 17:01:11 +0100

    clean

--- api/downloadVideo.js
+++ api/downloadVideo.js
@@ -4,7 +4,6 @@ const fileSize = require('./fileSize')
 const Axios = require('axios')
 
 const getFilesizeInBytes = filename => {
-    // console.log('stats', filename);
     let p = Path.resolve(__dirname, filename);
     return fs.existsSync(p) ? fs.statSync(p)["size"] : 0;
 };
@@ -12,12 +11,10 @@ const getFilesizeInBytes = filename => {
 const downloadVideo = async (url, dest, ms, multibar) => {
     // console.log('URL', url);
     const remoteFileSize = await fileSize(url);
-    //ms.add(url, { text: `Checking if video is downloaded: ${dest.split('/').pop()}` });
-    /*if (remoteFileSize == getFilesizeInBytes(`${dest}`)) {
-        //ms.succeed(url, { text: `Video exists: ${remoteFileSize}, ${getFilesizeInBytes(`${dest}`)} dest: ${dest}` });
+    if (remoteFileSize == getFilesizeInBytes(`${dest}`)) {
         console.log(`Video exists: ${remoteFileSize}, ${getFilesizeInBytes(`${dest}`)} dest: ${dest}`);
         return Promise.resolve();
-    }*/
+    }
 
     const { data, headers } = await Axios({
         url,
@@ -25,32 +22,24 @@ const downloadVideo = async (url, dest, ms, multibar) => {
         responseType: 'stream'
     })
     const totalLength = headers['content-length']
-
-
-    const m = multibar.create(totalLength, 0, {file: dest});
-    //const b2 = multibar.create(1000, 0);
+    const m = multibar.create(totalLength, 0, { file: dest });
 
     const writer = fs.createWriteStream(
         Path.resolve(__dirname, dest)
     )
     let len = 0;
     data.on('data', (chunk) => {
-        // console.log('chunk', chunk);
-        //ms.update(url, {text : `chunk length: ${chunk.length}`})
-        //progressBar.tick(chunk.length)
         m.update(len += chunk.length);//, {filename: "helloworld.txt"}
     })
     data.pipe(writer)
 
     return new Promise((resolve, reject) => {
         writer.on('finish', () => {
-            //ms.succeed(url, { text: `Video download url: ${url}` });//
             m.stop()
             return resolve();
         })
         writer.on('error', reject)
     })
-
 }
 
 
