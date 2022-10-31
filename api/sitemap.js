d1d947f05ec1d445b2fda2dd0fd28fb48ee2c5b4
Muhamed Didovic <muhamed.didovic@gmail.com>
2022-10-31 22:42:09 +0100

    update sitemap genrator

--- api/sitemap.js
+++ api/sitemap.js
@@ -21,7 +21,7 @@ async function getSitemapUrls() {
                         regex.lastIndex++;
                     }
                     m.forEach((match) => {
-                        if (match.indexOf('<') < 0){
+                        if (match.indexOf('<') < 0 && match.length){
                             console.log('match', match);
                             out.push(`https://www.vuemastery.com/courses/` + match);
                         }
