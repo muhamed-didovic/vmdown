b24b5c8ed0a1b9a6877a709405c68b8fa1e4ae28
Muhamed Didovic <muhamed.didovic@gmail.com>
2023-01-17 10:59:52 +0100

    update nightmare test

--- test/nightmare/createPageCapturer.js
+++ test/nightmare/createPageCapturer.js
@@ -46,36 +46,27 @@ const noop = () => {}
 test.after.always(() => {
     if (browser) browser.end()
     // imgs.forEach(imgPath => fs.unlink(imgPath, noop))
-    fs.removeSync(path.join(__dirname,  'intro-to-vue-js'),  { recursive: true, force: true });//, { recursive: true, force: true }
+    fs.removeSync(path.join(__dirname,  'intro-to-vue-3'),  { recursive: true, force: true });//, { recursive: true, force: true }
 })
 
 
-test.skip('capturePage nightmare', async t => {
+test('capturePage nightmare', async t => {
     console.log('11111');
-    const res1 = await scrape('https://www.vuemastery.com/courses/intro-to-vue-js/vue-instance', '720p')
+    const res1 = await scrape('https://www.vuemastery.com/courses/intro-to-vue-3/intro-to-vue3', '720p')
     console.log('res1', res1);
-    const res2 = await scrape("https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding");
-    console.log('res2', res2);
-    const res3 = await scrape("https://www.vuemastery.com/courses/intro-to-vue-js/conditional-rendering");
-    console.log('res3', res3);
 
-    t.true(fs.existsSync(res1.imgPath))
-    t.true(fs.existsSync(path.join(__dirname, 'intro-to-vue-js', 'nightmare', 'markdown', '1. The Vue Instance.md')))
-    t.is(res1.pageUrl, 'https://www.vuemastery.com/courses/intro-to-vue-js/vue-instance')
-    t.is(res1.courseName, 'intro-to-vue-js')
-    t.truthy(res1.vimeoUrl)
+    t.is(res1.length, 11)
+    t.true(fs.existsSync(res1[0].imgPath))
+    t.true(fs.existsSync(path.join(__dirname, 'intro-to-vue-3', 'nightmare', 'markdown', '1. Intro to Vue 3.md')))
+    t.is(res1[0].pageUrl, 'https://www.vuemastery.com/courses/intro-to-vue-3/intro-to-vue3')
+    t.is(res1[0].courseName, 'intro-to-vue-3')
+    t.truthy(res1[0].vimeoUrl)
+    // fs.copyFileSync(res1[0].imgPath, path.join(__dirname, '..', 'snapshot/capture1.png'))
+
+    // t.true(fs.existsSync(res1.imgPath))
+    // t.true(fs.existsSync(path.join(__dirname, 'intro-to-vue-js', 'nightmare', 'markdown', '1. The Vue Instance.md')))
+    // t.is(res1.pageUrl, 'https://www.vuemastery.com/courses/intro-to-vue-js/vue-instance')
+    // t.is(res1.courseName, 'intro-to-vue-js')
+    // t.truthy(res1.vimeoUrl)
     // fs.copyFileSync(res1.imgPath, path.join(__dirname, '..', 'snapshot/capture1.png'))
-
-    t.true(fs.existsSync(res2.imgPath))
-    t.true(fs.existsSync(path.join(__dirname, 'intro-to-vue-js', 'nightmare', 'markdown', '2. Attribute Binding.md')))
-    t.is(res2.pageUrl, 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding')
-    // console.log('aaaaa', path.join(__dirname, 'snapshot/capture2.png'));
-    // fs.copyFileSync(res2.imgPath, path.join(__dirname, '..', 'snapshot/capture2.png'))
-
-    t.true(fs.existsSync(res3.imgPath))
-    t.true(fs.existsSync(path.join(__dirname, 'intro-to-vue-js', 'nightmare', 'markdown', '3. Conditional Rendering.md')))
-    t.is(res3.pageUrl, 'https://www.vuemastery.com/courses/intro-to-vue-js/conditional-rendering')
-    // fs.copyFileSync(res3.imgPath, path.join(__dirname, '..', 'snapshot/capture3.png'))
-
-    t.true(true)
 })
